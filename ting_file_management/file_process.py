25881ef9b8c02e2e2f87e2157aa7a606dac213d3
wallaC-137 <wallace_031@hotmail.com>
2023-07-16 11:32:57 -0300

    req03: ok

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,36 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        search = instance.search(i)
+        if search["nome_do_arquivo"] == path_file:
+            return
+
+    text_lines = txt_importer(path_file)
+
+    file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text_lines),
+        "linhas_do_arquivo": text_lines,
+    }
+
+    instance.enqueue(file_dict)
+    print(file_dict, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not len(instance):
+        return print("Não há elementos", file=sys.stdout)
+
+    file_removed = instance.dequeue()
+    file_name = file_removed["nome_do_arquivo"]
+    print(f"Arquivo {file_name} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        print(instance.search(position), file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
