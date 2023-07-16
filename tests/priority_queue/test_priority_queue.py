4330da6ffee33f10208927f88bfcd7e57b0503c8
wallaC-137 <wallace_031@hotmail.com>
2023-07-16 17:39:06 -0300

    req07: ok

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,22 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from pytest import raises
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+
+    pq.enqueue({"nome_do_arquivo": "arquivo1.txt", "qtd_linhas": 10})
+    pq.enqueue({"nome_do_arquivo": "arquivo2.txt", "qtd_linhas": 3})
+    pq.enqueue({"nome_do_arquivo": "arquivo3.txt", "qtd_linhas": 7})
+    assert len(pq) == 3
+
+    assert pq.dequeue() == {"nome_do_arquivo": "arquivo2.txt", "qtd_linhas": 3}
+    assert pq.dequeue() == {"nome_do_arquivo": "arquivo3.txt", "qtd_linhas": 7}
+    assert pq.dequeue() == {
+        "nome_do_arquivo": "arquivo1.txt",
+        "qtd_linhas": 10,
+    }
+    assert len(pq) == 0
+
+    with raises(IndexError):
+        pq.search(7)
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
+from ting_file_management.queue import Queue
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_word = []
+    for file_dict in instance._queue:
+        file_name = file_dict["nome_do_arquivo"]
+        lines = file_dict["linhas_do_arquivo"]
+
+        occurrences = []
+        for line_num, line in enumerate(lines, start=1):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": line_num})
+
+        if occurrences:
+            found_word.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return found_word
 
 
 def search_by_word(word, instance):
