60fa9c4f1388196f2faa0985544801e09f810c3b
wallaC-137 <wallace_031@hotmail.com>
2023-07-16 10:48:19 -0300

    req01: ok - implementa a classe Queue e seu metodos __len__, enqueue, dequeue, search/ req02: ok - implementa a função txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return None
+    try:
+        with open(path_file, "r") as file:
+            return file.read().split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,18 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._queue = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self._queue):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._queue[index]
