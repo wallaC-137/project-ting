739ec349718401de715636f815fa0d6f9171e2d6
wallaC-137 <wallace_031@hotmail.com>
2023-07-17 12:08:14 -0300

    req08: ok

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,6 +22,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    found_word = exists_word(word, instance)
-    if not found_word:
+    found_w = exists_word(word, instance)
+    if not found_w:
         return []
+
+    found_word = []
+    for file_dict in instance._queue:
+        file_name = file_dict["nome_do_arquivo"]
+        lines = file_dict["linhas_do_arquivo"]
+
+        occurrences = []
+        for line_num, line in enumerate(lines, start=1):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": line_num, "conteudo": line})
+
+        print(f"resultado espera: {occurrences}")
+
+        found_word.append(
+            {"palavra": word, "arquivo": file_name, "ocorrencias": occurrences}
+        )
+
+    return found_word
