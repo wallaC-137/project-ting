bb62eebafcc0abc3d3f00006a35d811cbdca01d7
wallaC-137 <wallace_031@hotmail.com>
2023-08-14 13:16:04 -0300

    test

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,31 +1,4 @@
-def exists_word(word, instance):
-    found_word = []
-    for file_dict in instance._queue:
-        file_name = file_dict["nome_do_arquivo"]
-        lines = file_dict["linhas_do_arquivo"]
-
-        occurrences = []
-        for line_num, line in enumerate(lines, start=1):
-            if word.lower() in line.lower():
-                occurrences.append({"linha": line_num})
-
-        if occurrences:
-            found_word.append(
-                {
-                    "palavra": word,
-                    "arquivo": file_name,
-                    "ocorrencias": occurrences,
-                }
-            )
-
-    return found_word
-
-
-def search_by_word(word, instance):
-    found_w = exists_word(word, instance)
-    if not found_w:
-        return []
-
+def test(word, instance, content=1):
     found_word = []
     for file_dict in instance._queue:
         file_name = file_dict["nome_do_arquivo"]
@@ -38,8 +11,49 @@ def search_by_word(word, instance):
 
         print(f"resultado espera: {occurrences}")
 
-        found_word.append(
-            {"palavra": word, "arquivo": file_name, "ocorrencias": occurrences}
-        )
+        if occurrences:
+            found_word.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    # deletar chave content do found_word
+    if content == 0:
+        for found in occurrences:
+            del found["conteudo"]
 
     return found_word
+
+
+def exists_word(word, instance):
+    # found_word = []
+    # for file_dict in instance._queue:
+    #     file_name = file_dict["nome_do_arquivo"]
+    #     lines = file_dict["linhas_do_arquivo"]
+
+    #     occurrences = []
+    #     for line_num, line in enumerate(lines, start=1):
+    #         if word.lower() in line.lower():
+    #             occurrences.append({"linha": line_num})
+
+    #     if occurrences:
+    #         found_word.append(
+    #             {
+    #                 "palavra": word,
+    #                 "arquivo": file_name,
+    #                 "ocorrencias": occurrences,
+    #             }
+    #         )
+
+    return test(word, instance, content=0)
+
+
+def search_by_word(word, instance):
+    found = test(word, instance)
+    if not found:
+        return []
+
+    return test(word, instance)
